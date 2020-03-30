30c9a71273460e27c2b1ae9c9a89dabd6191b2c4
Indunil1992 <indunil@adroitlogic.com>
2020-03-30 18:18:55 +0530

    1

--- tranlateindu30.js
+++ tranlateindu30.js
@@ -4,12 +4,12 @@ const translate = new AWS.Translate();
 exports.handler = async (event) => {
     try {
         let data = await translate.translateText({
-            SourceLanguageCode: "auto",
-            TargetLanguageCode: "sv",
-            Text: "mother"
+            SourceLanguageCode: "sv",
+            TargetLanguageCode: "en",
+            Text: "mor"
         }).promise();
         console.log("dattttaa")
-console.log(data)
+        console.log(data)
     } catch (err) {
         console.log(err)
         // error handling goes here
